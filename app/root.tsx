2f8bf546fbff0a64c436159ecaa7625de0dc14f1
Matias Leidemer <matiasleidemer@gmail.com>
2023-01-10 01:14:16 +0000

    Configure tailwindcss

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,12 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import styles from "./styles/app.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
+
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
@@ -21,7 +27,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-blue-500">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
